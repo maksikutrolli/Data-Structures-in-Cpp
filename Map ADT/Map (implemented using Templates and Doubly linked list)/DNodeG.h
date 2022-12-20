0de59d55fc4625d40c8c7fd27fc7fe4924cdb1d0
mkutrolli <95157493+maksikutrolli@users.noreply.github.com>
2022-12-20 06:49:57 +0100

    Add files via upload

---
+++ Map ADT/Map (implemented using Templates and Doubly linked list)/DNodeG.h
@@ -0,0 +1,34 @@
+/*
+ *      Author: MaksiKutrolli
+ */
+
+#ifndef DNODEG_H_
+#define DNODEG_H_
+
+#include <iostream>
+
+using namespace std;
+
+template <typename K, typename V>
+class NodeMapG;
+
+template<typename K, typename V >
+class IteratorG;
+
+template<typename K, typename V >
+class Entry;
+
+template <typename K, typename V>
+class DNodeG{
+private:
+	Entry<K,V> elm;
+	DNodeG<K,V> *next;
+	DNodeG<K,V> *prev;
+
+	friend class NodeMapG<K,V>;
+	friend class IteratorG<K,V>;
+};
+
+
+
+#endif /* DNODEG_H_ */
---
+++ Map ADT/Map (implemented using Templates and Doubly linked list)/Main.cpp
@@ -0,0 +1,69 @@
+/*
+ *      Author: MaksiKutrolli
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+#include "NodeMapG.h"
+
+//#include "DNodeG.h"
+#include "IteratorG.h"
+int main(){
+
+//	NodeMapG<int,int> mm();
+
+	NodeMapG<int,int> t1;
+
+	t1.put(8, 5);
+
+	t1.put(2, 5);
+
+	t1.put(3, 7);
+
+	t1.put(2, 3);
+
+	t1.print();
+
+	std::cout<<*t1.find(3)<<endl;
+
+	IteratorG<int,int> it;
+
+	it = t1.find(8);
+
+	std::cout<<*it<<endl;
+
+
+//	Entry<int,int> entry(2,3);
+
+//	t1.put(2,4);
+
+//	IteratorG<int,int> it;
+//	it = t1.put(2,4);
+
+//	std::cout<<*it;
+
+//	t1.erase(2);
+
+
+
+//	Entry<int,int> entry;
+//
+//	entry.setKey(2);
+//	entry.setValue(5);
+//
+//	cout<<entry.valuee();
+//
+//
+//
+//
+//
+//	NodeMapG<int, int> t2;
+
+
+
+
+	return 0;
+}
