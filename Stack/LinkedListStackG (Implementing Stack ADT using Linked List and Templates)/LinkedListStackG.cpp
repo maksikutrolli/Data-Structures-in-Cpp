ece0eaf76f5cbeab08130b3eed722919f146832f
mkutrolli <95157493+maksikutrolli@users.noreply.github.com>
2023-10-04 04:20:29 +0200

    Update LinkedListStackG.cpp

--- Stack/LinkedListStackG (Implementing Stack ADT using Linked List and Templates)/LinkedListStackG.cpp
+++ Stack/LinkedListStackG (Implementing Stack ADT using Linked List and Templates)/LinkedListStackG.cpp
@@ -11,36 +11,33 @@ LinkedListStackG<T, R>::LinkedListStackG(): S(), n(0)  {
 
 template<typename T, typename R>
 LinkedListStackG<T, R>::~LinkedListStackG() {
+	// TODO
 }
 
 template<typename T, typename R>
 int LinkedListStackG<T, R>::size() const {
-	return n;
+	// TODO
 }
 
 template<typename T, typename R>
 bool LinkedListStackG<T, R>::empty() const {
-	return (n==0);
+	// TODO
 }
 
 template<typename T, typename R>
 const T& LinkedListStackG<T, R>::top() const throw (R) {
-	if (empty()) throw R("Top of empty stack");
-	return S.front();
+	// TODO
 }
 
 template<typename T, typename R>
 void LinkedListStackG<T, R>::push(const T &e) {
-	S.addFront(e);
-	n++;
+	// TODO
 }
 
 template<typename T, typename R>
 void LinkedListStackG<T, R>::pop() throw (R) {
-	if(empty()) throw R("Pop from empty stack");
-	n--;
-	S.removeFront();
+	// TODO
 }
 
-template class LinkedListStackG<int, StackException>;
-template class LinkedListStackG<string, StackException>;
+// TODO
+// TODO
