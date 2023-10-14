3c84567344b1a43153b490d703758deab58fbc81
mkutrolli <95157493+maksikutrolli@users.noreply.github.com>
2023-10-14 18:41:04 +0200

    Update ArrayStackG.cpp

--- Stack/ArrayStackG (Implementing Stack ADT using Arrays and Templates)/ArrayStackG.cpp
+++ Stack/ArrayStackG (Implementing Stack ADT using Arrays and Templates)/ArrayStackG.cpp
@@ -6,45 +6,52 @@
 
 #include "StackException.h"
 
-#include <iostream> 
+#include <iostream>
 
 using namespace std;
 
 template <typename T, typename R>
 ArrayStackG<T,R>::ArrayStackG(int cap): S(new T[cap]), capacity(cap), t(-1) {
 	// TODO Auto-generated constructor stub
+
 }
 
 
 template <typename T, typename R>
 ArrayStackG<T,R>::~ArrayStackG() {
-	// TODO Auto-generated destructor stub 
+	// TODO Auto-generated destructor stub
 }
 
 template <typename T, typename R>
 int ArrayStackG<T,R>::size()const{
-	// TODO
+	return t+1;
 }
 
 template <typename T, typename R>
 bool ArrayStackG<T,R>::empty()const{
-	// TODO
+	return (t < 0);
 }
 
 template <typename T, typename R>
 const T& ArrayStackG<T,R>::top() const throw(R){
-	// TODO
+	if (empty()) throw R("Top of empty stack");
+	return S[t];
 }
 
 template <typename T, typename R>
 void ArrayStackG<T,R>::push(const T& e){
-	// TODO
+	if (size() == capacity) throw R("Push to full stack");
+	t++;
+	S[t] = e;
+
 }
 
 template <typename T, typename R>
 void ArrayStackG<T,R>::pop() throw(R){
-	// TODO
+	if (empty()) throw R("Pop from empty stack");
+	--t;
+
 }
 
-// TODO
-// TODO
+template class ArrayStackG<int, StackException>;
+template class ArrayStackG<string, StackException>;
